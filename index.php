63e7cd695c070454b5de6ef9036d9b4fb23fa6bb
David Irvine <david.irvine@maidsafe.net>
2014-01-23 00:36:08 +0000

    playnig about

--- index.php
+++ index.php
@@ -1,7 +1,4 @@
-<?php $title="Distributed Platform, MaidSafe"; ?>
-<?php $metakeywords="Distributed Platform, MaidSafe"; ?>
-<?php $metadescription="MaidSafe's distributed platform enables the creation of fast and secure applications that help ensure digital privacy, security and freedom for all."; ?>
-<?php include('includes/header.php'); ?>
+											var title="Distributed Platform, MaidSafe"; var metakeywords="Distributed Platform, MaidSafe"; var metadescription="MaidSafe's distributed platform enables the creation of fast and secure applications that help ensure digital privacy, security and freedom for all."; 
 <body id="home">
 <div id="container">
     <div id="main">
